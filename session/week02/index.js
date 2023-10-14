637d57d10ac11a4c4da0ff07d99fb1eafdea5b8f
Nayujin <00nyj@sookmyung.ac.kr>
2023-10-14 17:33:15 +0900

    [week02] 망고 node 추가 후 빨간 과일 node들 삭제

---
+++ session/week02/index.js
@@ -0,0 +1,20 @@
+console.log('hi');
+const mango = document.createElement("li");
+
+// 텍스트 추가 방법 1
+const mangoText=document.createTextNode('mango');
+
+// 텍스트 추가 방법 2
+// mango.innerText = "mango";
+
+mango.appendChild(mangoText);
+
+const fruitList=document.querySelector("ul");
+
+fruitList.appendChild(mango);
+
+const redFruit= document.querySelectorAll(".red");
+// console.log(redFruit);
+redFruit.forEach((fruit)=>{
+    fruit.remove(); // 화살표 방식의 콜백함수를 사용한 것
+})
