2946fd5435d0a6c4beab33fa342ead5606f982be
Nayujin <00nyj@sookmyung.ac.kr>
2023-10-27 18:40:03 +0900

    feat: 이미지 hover시 설명 visible

---
+++ assignment/week02/assign1/index.js
@@ -0,0 +1,31 @@
+const sec1ImgList = document.querySelectorAll(".sec1wrap img");
+
+sec1ImgList.forEach((img)=>{
+    let title=img.getAttribute('img-title');
+    let desc=img.getAttribute('img-description');
+    const spantitle = document.createElement('span');
+    const spandesc = document.createElement('span');
+    spantitle.classList.add('title');
+    spandesc.classList.add('content');
+    spantitle.appendChild(document.createTextNode(title));
+    spandesc.appendChild(document.createTextNode(desc));
+    const wrapper=img.parentNode;
+
+    wrapper.append(spantitle);
+    wrapper.append(spandesc);
+    img.addEventListener('mouseover', showContent);
+    
+})
+
+function showContent(event){    
+    console.log(event.target.nextElementSibling);
+    const title=event.target.nextElementSibling;
+    title.style.visibility="visible";
+    const desc=event.target.nextElementSibling.nextElementSibling;
+    desc.style.visibility="visible";
+}
+
+
+
+
+
