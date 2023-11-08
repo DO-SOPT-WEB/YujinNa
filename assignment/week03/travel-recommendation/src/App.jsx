025fc19ae4aba1b1fbc4613210dc45c635bc55dd
nayujin-dev <00skdbwls@gmail.com>
2023-11-09 02:05:03 +0900

    feat: 추천 종류 선택

--- assignment/week03/travel-recommendation/src/App.jsx
+++ assignment/week03/travel-recommendation/src/App.jsx
@@ -3,14 +3,30 @@ import Header from './components/Header'
 import styled from 'styled-components'
 import Description from './components/Description'
 function App() {
-
+  const categoryList=['취향대로 추천', '랜덤 추천'];
+  const [category,setCategory]=useState(-1);
+  const onTypeClick=(category)=>{
+    category==='취향대로 추천'?setCategory('취향대로 추천'):setCategory('랜덤 추천');
+  }
   return (
     <>
       <Header/>
       <Container>
         <Description/>
         <MainBox>
-          ㅎㅎ
+          {category==-1 ? (
+            categoryList.map((type,index)=>
+            <TypeofRecommand key={index} onClick={(e)=>onTypeClick(type,e)}>
+              {type}
+            </TypeofRecommand>)
+          ):(
+            <TypeofRecommand>
+              {category}
+            </TypeofRecommand>
+          )
+          
+          }
+          
         </MainBox>
         <BtnBox>
           버튼들 위치
@@ -37,10 +53,12 @@ const Container=styled.div`
 const MainBox=styled.div`
   position: absolute;
   top: 2rem;
-  background-color: white;
+  /* background-color: white; */
   height: 20rem;
   margin: 3rem;
   padding: 1rem;
+  width: 60vw;
+  display: flex;
 `;
 
 const BtnBox=styled.div`
@@ -49,3 +67,12 @@ const BtnBox=styled.div`
   top: 30rem;
 `;
 
+const TypeofRecommand=styled.div`
+  width: 40em;
+  background-color: #ffe0e0;
+  margin: 0 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 1rem;
+`;
