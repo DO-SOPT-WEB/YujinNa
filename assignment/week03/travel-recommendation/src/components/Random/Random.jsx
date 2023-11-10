f9f2da56b5fec43b9d138a5768af585fa1b3b3e3
nayujin-dev <00skdbwls@gmail.com>
2023-11-10 17:39:15 +0900

    feat: 랜덤선택 카운트다운 구현

--- assignment/week03/travel-recommendation/src/components/Random/Random.jsx
+++ assignment/week03/travel-recommendation/src/components/Random/Random.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Random = () => {
+    const [time,setTime]=useState(3);
+    useEffect(()=>{
+        time>0&&setTimeout(()=>setTime(time-1),1000);
+    },[time]);
     return (
         <div>
-            
+            {time>0?(
+                time
+            ):(
+                <>결과</>
+            )}
         </div>
     );
 };
