a3bb3b580cf48054ef5bceaea67718c9b507ab2f
nayujin-dev <00skdbwls@gmail.com>
2023-11-17 15:33:59 +0900

    fix: 중복되는 styled-component 분리

--- assignment/week04/login-project/src/pages/Login.jsx
+++ assignment/week04/login-project/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import { St } from "../styles/Modal";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -31,9 +31,9 @@ const Login = () => {
     }
   };
   return (
-    <St.LoginContainer>
-      <St.LoginBox>
-        <St.LoginTitle>Login</St.LoginTitle>
+    <St.ModalContainer>
+      <St.ModalBox>
+        <St.ModalTitle>Login</St.ModalTitle>
         <St.InputContainer>
           <St.InputCategory>ID</St.InputCategory>
           <St.InputBox
@@ -59,79 +59,9 @@ const Login = () => {
             회원가입
           </St.RegisterButton> */}
         </St.ButtonContainer>
-      </St.LoginBox>
-    </St.LoginContainer>
+      </St.ModalBox>
+    </St.ModalContainer>
   );
 };
 
 export default Login;
-
-const St = {
-  LoginContainer: styled.div`
-    position: absolute;
-    top: 0;
-    left: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100vh;
-    background-color: rgba(0, 0, 0, 0.2);
-  `,
-  LoginBox: styled.div`
-    padding: 3rem;
-    position: relative;
-    width: 50rem;
-    height: 30rem;
-    display: flex;
-    flex-direction: column;
-    font-size: 5rem;
-    border-radius: 2rem;
-    background-color: ${({ theme }) => theme.colors.white};
-  `,
-  LoginTitle: styled.div`
-    font-size: 3rem;
-    margin-bottom: 2rem;
-    text-align: center;
-  `,
-  InputContainer: styled.div`
-    display: flex;
-    flex-direction: row;
-    margin: 0.5rem auto;
-  `,
-  InputCategory: styled.div`
-    width: 10rem;
-  `,
-  InputBox: styled.input`
-    width: 30rem;
-    padding: 1rem;
-  `,
-  ButtonContainer: styled.div`
-    width: 40rem;
-    margin: 1.5rem auto;
-    display: flex;
-    flex-direction: column;
-  `,
-  SubmitButton: styled.button`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: inherit;
-    font-size: 2rem;
-    padding: 1rem;
-    border-radius: 1rem;
-    color: ${({ theme }) => theme.colors.white};
-    background-color: ${({ theme }) => theme.colors.black};
-  `,
-  RegisterButton: styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: inherit;
-    font-size: 2rem;
-    padding: 1rem;
-    border-radius: 1rem;
-    color: ${({ theme }) => theme.colors.black};
-    background-color: ${({ theme }) => theme.colors.white};
-  `,
-};
---
+++ assignment/week04/login-project/src/styles/Modal.jsx
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+
+export const St = {
+  ModalContainer: styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.2);
+  `,
+  ModalBox: styled.div`
+    padding: 3rem;
+    position: relative;
+    width: 50rem;
+    height: 30rem;
+    display: flex;
+    flex-direction: column;
+    font-size: 5rem;
+    border-radius: 2rem;
+    background-color: ${({ theme }) => theme.colors.white};
+  `,
+  ModalTitle: styled.div`
+    font-size: 3rem;
+    margin-bottom: 2rem;
+    text-align: center;
+  `,
+  InputContainer: styled.div`
+    display: flex;
+    flex-direction: row;
+    margin: 0.5rem auto;
+  `,
+  InputCategory: styled.div`
+    width: 10rem;
+  `,
+  InputBox: styled.input`
+    width: 30rem;
+    padding: 1rem;
+  `,
+  ButtonContainer: styled.div`
+    width: 40rem;
+    margin: 1.5rem auto;
+    display: flex;
+    flex-direction: column;
+  `,
+  SubmitButton: styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: inherit;
+    font-size: 2rem;
+    padding: 1rem;
+    border-radius: 1rem;
+    color: ${({ theme }) => theme.colors.white};
+    background-color: ${({ theme }) => theme.colors.black};
+  `,
+  RegisterButton: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: inherit;
+    font-size: 2rem;
+    padding: 1rem;
+    border-radius: 1rem;
+    color: ${({ theme }) => theme.colors.black};
+    background-color: ${({ theme }) => theme.colors.white};
+  `,
+};
