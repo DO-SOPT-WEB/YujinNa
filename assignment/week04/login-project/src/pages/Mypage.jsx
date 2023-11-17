d19824be3b3239f0339d697bb5163f4aa58a4e4f
nayujin-dev <00skdbwls@gmail.com>
2023-11-17 15:26:58 +0900

    feat: 마이페이지 로직, axios 구현 완료

--- assignment/week04/login-project/src/pages/Mypage.jsx
+++ assignment/week04/login-project/src/pages/Mypage.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Mypage = () => {
-  return <div>hi</div>;
+  const { userId } = useParams();
+  const [username, setUserName] = useState("");
+  const [nickname, setNickName] = useState("");
+  useEffect(() => {
+    userId !== "" && getUserInfo();
+  }, [userId]);
+  const getUserInfo = () => {
+    try {
+      axios
+        .get(`${import.meta.env.VITE_BASE_URL}/api/v1/members/${userId}`)
+        .then((response) => {
+          console.log(response);
+          setUserName(response.data.username);
+          setNickName(response.data.nickname);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div>
+      <div>{username}</div>
+      <div>{nickname}</div>
+    </div>
+  );
 };
 
 export default Mypage;
